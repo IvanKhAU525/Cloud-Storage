7311fd12ea1b4f5f930fe8e3428e82780439acb0
Иван <ivanbesch@outlook.com>
2017-09-16 21:09:30 +0300

    First commit

---
+++ Cloud Storage/Cloud Storage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cloud_Storage.MvcApplication" Language="C#" %>
---
+++ Cloud Storage/Cloud Storage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cloud_Storage.Startup))]
+namespace Cloud_Storage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
